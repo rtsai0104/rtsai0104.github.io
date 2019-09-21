a2992438f372f39472d19f46e4a82f9f2918b73a
Ryan Tsai <rtsai0104@gmail.com>
2019-09-21 14:20:19 -0500

    Updates

---
+++ precache-manifest.5c69847e66fae9a67cec1f09c89c1de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f77c74c125280eda5078a22e9d3273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dfba531a55f06129cb5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9b798d8747f8b886d24d",
+    "url": "/static/js/2.64dbc533.chunk.js"
+  },
+  {
+    "revision": "7dfba531a55f06129cb5",
+    "url": "/static/js/main.e1ae5040.chunk.js"
+  },
+  {
+    "revision": "de08b1579d1bff677ea5",
+    "url": "/static/js/runtime-main.cabf1c38.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
